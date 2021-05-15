e6583fa37f3564d91316f229384cfb65169f3fe0
erbynn <john.erbynn@gmail.com>
2021-05-15 12:36:46 +0000

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from './interfaces/user';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demystify';
+  users: User[] = [];
+
+  constructor(private userService: UserService){
+  }
+
+  ngOnInit(): void {
+    this.userService.fetchUsers().subscribe(users => {
+      this.users = users
+    });
+  }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { User } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  fetchUsers() {
+    return this.http.get<User[]>("https://my-json-server.typicode.com/pkErbynn/user-service-mock/users");
+  }
+}
